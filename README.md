cd9c40c44d6f6022f2f1fa7c5c937621032a8ca3
Valentin Krasontovitsch <v.krasontov@gmail.com>
2017-11-09 23:38:33 +0100

    Feat: Add connect script

--- README.md
+++ README.md
@@ -3,12 +3,25 @@ some shell scripts to run a radio through a bluetooth speaker
 
 ## Config
 
-env vars... rename!
+Various pieces of information can be configured through environment variables,
+such as:
+
+- `SPEAKER_ADDRESS` MAC address of the bluetooth speaker (mandatory)
+- `CONNECT_TRIALS` number of tries for connecting before giving up (mandatory)
+
 
 ## Usage
 
 TODO describe
 
-## Prerequisites
+## Requirements
 
-TODO reconstruct
+These scripts use the following binaries, which hence must be installed:
+
+- `bash`
+- `bluetoothctl`
+- `xargs`
+
+TODO which packages should one install to have this running?
+Which steps taken to configure everything? is trusting necessary?
+how about scanning, ... ?
---
+++ connect.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# exit on non zero return
+set -e
+# error if unbound variable used
+set -u
+
+if [ -z "$SPEAKER_ADDRESS" ]; then
+  echo "bluetooth speaker address not set!"
+  exit 1
+fi
+
+CONNECTED=$(echo "info $SPEAKER_ADDRESS" | bluetoothctl 2>&1 | xargs -L 1 echo \
+  | grep -e Connected: | awk '{ print $2 }')
+
+if [ "$CONNECTED" == "yes" ]; then echo 'Already connected!' && exit 0; fi
+
+CONN_MAX_TRY=$CONNECT_TRIALS
+
+echo "Will try to connext $trials time(s)..."
+echo
+
+trial=1
+while true; do
+  echo Attempting to connect ...
+  echo "connect $SPEAKER_ADDRESS" | bluetoothctl 2>&1 | xargs -L 1 echo
+
+  CONNECTED=$(echo "info $SPEAKER_ADDRESS" | bluetoothctl 2>&1 | xargs -L 1 echo \
+    | grep -e Connected: | awk '{ print $2 }')
+
+  if [ "$CONNECTED" == "yes" ]; then
+    echo We are connected!
+    break
+  else
+    echo Failed to connect... Push scan button on speaker?
+    echo Trial \# $trial
+    if [ $trial -eq $CONN_MAX_TRY ]; then
+      echo Reached limit of $CONN_MAX_TRY failed connection trials
+      exit 0
+    fi
+  fi
+  let "trial += 1"
+  sleep 1
+done
