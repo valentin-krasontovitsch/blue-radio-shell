c61fc72dd81ab422c7631a9b8e875e24615b36b8
Valentin Krasontovitsch <v.krasontov@gmail.com>
2021-01-05 21:08:28 +0100

    Fix: Typo

--- get_volume.sh
+++ get_volume.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-VOLUME=$(amizer sget 'Master' | tail -n1 | sed -E 's/.*\[([0-9]+)%\].*/\1/')
+VOLUME=$(amixer sget 'Master' | tail -n1 | sed -E 's/.*\[([0-9]+)%\].*/\1/')
 echo $VOLUME
 exit 0
